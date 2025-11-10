bb9cc7598546ca3d77c7d1d248043c439140d398
ym <443929389@qq.com>
2025-11-10 16:29:15 +0800

    costomized pipe reservepipe

--- src/pip-demo/pip-demo.ts
+++ src/pip-demo/pip-demo.ts
@@ -1,15 +1,16 @@
 import {Component} from '@angular/core';
 import {LowerCasePipe,DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
+import {ReversePipe} from './reserve.pipe';
 
 @Component({
   selector: 'app-pip-demo',
   templateUrl:'./pip-demo.html',
-  imports: [LowerCasePipe, DecimalPipe, DatePipe, CurrencyPipe],
+  imports: [LowerCasePipe, DecimalPipe, DatePipe, CurrencyPipe,ReversePipe],
 })
 
 export class PipDemo {
   username = 'yOunGTECh';
   birthday = new Date(2000,5,20);
   cash = 2000000;
-  pi:number = 2.14159261111111;
+  pi:number = 3.1415926;
 }
